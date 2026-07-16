25057f30fe355322b66bacd36663d83364164baf
Khulood Noor <KhuloodNoor@gmail.com>
2026-07-16 02:37:12 -0700

    UI updated

--- lib/viewModel/category_view_news_model.dart
+++ lib/viewModel/category_view_news_model.dart
@@ -7,7 +7,13 @@ class CategoryViewNewsModel{
 
   Future<CategoryNewsModel> fetchCategoryNews(String category)async{
     String uri="https://newsapi.org/v2/everything?q=$category&apiKey=8a5ec37e26f845dcb4c2b78463734448";
-    final response=await http.get(Uri.parse(uri));
+    final response = await http.get(
+      Uri.parse(uri),
+      headers: {
+        'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+      },
+    );
     if(response.statusCode==200){
       final body=jsonDecode(response.body);
       return CategoryNewsModel.fromJson(body);
--- lib/viewModel/news_view_model.dart
+++ lib/viewModel/news_view_model.dart
@@ -10,7 +10,13 @@ class NewsViewModel {
     String uri =
         "https://newsapi.org/v2/top-headlines?sources=${channel}&apiKey=98dee3928461470d8f4fcbbfbc664f4a";
 
-    final response = await http.get(Uri.parse(uri));
+    final response = await http.get(
+      Uri.parse(uri),
+      headers: {
+        'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+      },
+    );
 
     if (response.statusCode == 200) {
       final body = jsonDecode(response.body);
